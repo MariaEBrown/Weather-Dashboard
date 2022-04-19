1fb6fb76dcdb63cbcac8497e365216e98cdb192f
Maria Brown <mariabrown0908@gmail.com>
2022-04-18 23:59:28 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,9 @@
+var APIKey = "eda05757228968d11b4d33249ae0d82c";
+var city; Boston
+
+api.openweathermap.org/data/2.5/weather?q=Bosston&appid={"eda05757228968d11b4d33249ae0d82c"};
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
+
