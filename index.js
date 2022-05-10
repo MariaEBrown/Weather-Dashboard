ff982d4b36cc3919ac0d4d610e5cd9bfb995c2dd
Maria Brown <mariabrown0908@gmail.com>
2022-05-09 22:36:17 -0400

    updating html and js file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var APIKey = "eda05757228968d11b4d33249ae0d82c";
-var city; Boston
 
-api.openweathermap.org/data/2.5/weather?q=Bosston&appid={"eda05757228968d11b4d33249ae0d82c"};
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-fetch(queryURL)
+let weather = {
+    "apiKey": "eda05757228968d11b4d33249ae0d82c",
+    fetchWeather: function () {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=Boston&units=metric&appid=eda05757228968d11b4d33249ae0d82c')
+        .then ((response) => response.json ())
+        .then((data) => console.log(data));
+    },
+    
+};
 
